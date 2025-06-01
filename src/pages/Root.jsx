75361d8fc7bd0ce932b9e845e90f8bc2a4358c51
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-06-02 00:34:46 +0300

    Using Typography

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Typography from '@mui/material/Typography'
 
 const Root = () => {
     return (
         <div>
             <header>
-                MY header 
+               <Typography noWrap gutterBottom variant='h4'>
+                mmmmm  mmmmm  mmmmm  mmmmm  mmmmm  mmmmm
+               </Typography>
+               <h1>ssssssss</h1>
             </header>
 
 {/* بنستخدمها عشان نعرض المكون الاساسي 
