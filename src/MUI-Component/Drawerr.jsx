f344eb9bcd0e260857a116ae71ddd818d1c8fe64
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-06-02 14:06:53 +0300

    Use List from MUI

--- src/MUI-Component/Drawerr.jsx
+++ src/MUI-Component/Drawerr.jsx
@@ -2,28 +2,84 @@ import {
   Toolbar,
   Drawer,
   Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
 } from "@mui/material";
+import HomeIcon from "@mui/icons-material/Home";
+import CreateIcon from "@mui/icons-material/Create";
+import PersonIcon from "@mui/icons-material/Person";
+import SettingsIcon from "@mui/icons-material/Settings";
+import LogoutIcon from "@mui/icons-material/Logout";
 
-
-
-const Drawerr = ({drawerWidth}) => {
-    return (
-            <Drawer
-        sx={{
+const Drawerr = ({ drawerWidth }) => {
+  return (
+    <Drawer
+      sx={{
+        width: `${drawerWidth}px`,
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
           width: `${drawerWidth}px`,
-          flexShrink: 0,
-          "& .MuiDrawer-paper": {
-            width: `${drawerWidth}px`,
-            boxSizing: "border-box",
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-        <Toolbar />
-        <Divider />
-      </Drawer>
-    );
-}
+          boxSizing: "border-box",
+        },
+      }}
+      variant="permanent"
+      anchor="left"
+    >
+      <Toolbar />
+      <Divider />
+
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CreateIcon />
+            </ListItemIcon>
+            <ListItemText primary="Create" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <PersonIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+
+      </List>
+    </Drawer>
+  );
+};
 
 export default Drawerr;
