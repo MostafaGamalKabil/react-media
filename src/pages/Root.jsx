1fdb9bbf6570819a360744b81cb75140b94542db
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-06-02 11:13:34 +0300

    use avatar & Link from MUI

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -1,48 +1,27 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
-import DeleteIcon from "@mui/icons-material/Delete";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import { Typography, AppBar, Toolbar, Link, Avatar } from "@mui/material";
+
 const Root = () => {
   return (
     <div>
-      <header>
-        {/* 
-                there 3 ways to style MUI Elements
-                1- add className and style it
-                2- using sx Atribute (inline style)
-                3- add Atribute that can style your Elment
-                */}
-        <Typography
-          className="title"
-          mt={1}
-          border="5px solid red"
-          sx={{ backgroundColor: "royalblue", color: "white" }}
-          noWrap
-          gutterBottom
-          variant="h4"
-        >
-          mmmmm mmmmm mmmmm mmmmm mmmmm mmmmm
-        </Typography>
-        {/* How to use Button in MUI */}
-        <Button
-          sx={{ ml: "auto", mr: "auto", display: "flex" }}
-          endIcon={<DeleteIcon />}
-          variant="outlined"
-          color="primary"
-        >
-          Button
-        </Button>
+      <AppBar position="static">
+        <Toolbar>
+          <Link
+            sx={{ flexGrow: 1, textDecoration: "none" }}
+            color="inherit"
+            href="/"
+          >
+            My expenses
+          </Link>
 
-        {/* How to use IconButton in MUI */}
+          <Typography mr={2} variant="body2" component="div">
+            Mostafa gamal
+          </Typography>
 
-        <IconButton size="large">
-          {/* Icon That You import */}
-          <MenuIcon fontSize="inherit" />
-        </IconButton>
-      </header>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/photo.jpg" />
+        </Toolbar>
+      </AppBar>
 
       {/* بنستخدمها عشان نعرض المكون الاساسي 
      مع باقي المكونات اللي هروح لصفحتها(Root)
