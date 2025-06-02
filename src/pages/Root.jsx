4cec2bcc91e51648de2a34f0d32fe0273e538343
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-06-02 07:11:19 +0300

    Styling MUI Elements

--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -6,10 +6,22 @@ const Root = () => {
     return (
         <div>
             <header>
-               <Typography noWrap gutterBottom variant='h4'>
+                {/* 
+                there 3 ways to style MUI Elements
+                1- add className and style it
+                2- using sx Atribute (inline style)
+                3- add Atribute that can style your Elment
+                */}
+               <Typography
+               className="title"
+               mt={1} 
+               border="5px solid red"
+               sx={{backgroundColor:"royalblue",color:"white"}} noWrap gutterBottom variant='h4'>
                 mmmmm  mmmmm  mmmmm  mmmmm  mmmmm  mmmmm
+                
                </Typography>
                <h1>ssssssss</h1>
+                
             </header>
 
 {/* بنستخدمها عشان نعرض المكون الاساسي 
