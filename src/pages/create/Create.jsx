c71d0815624e6c480b8d43aa7b723ca6411a508d
Mostafa Kabil <kabilmostafa060@gmail.com>
2025-06-10 02:10:36 +0300

    Send Data to db.json

--- src/pages/create/Create.jsx
+++ src/pages/create/Create.jsx
@@ -1,24 +1,29 @@
-import { Box, Button, InputAdornment, styled, TextField, IconButton } from "@mui/material";
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import { Box, Button, InputAdornment, styled, TextField } from "@mui/material";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import "./Create.css";
 
-import React from "react";
 import { purple } from "@mui/material/colors";
+import { useState } from "react";
 const ColorButton = styled(Button)(({ theme }) => ({
   color: theme.palette.getContrastText(purple[500]),
   backgroundColor: purple[500],
-  '&:hover': {
+  "&:hover": {
     backgroundColor: purple[700],
   },
 }));
 const Create = () => {
+  const [title, settitle] = useState("");
+  const [price, setprice] = useState(0);
   return (
-    <Box component="form" sx={{ width: "380px" }} >
+    <Box component="form" sx={{ width: "380px" }}>
       <TextField
+        onChange={(eo) => {
+          settitle(eo.target.value);
+        }}
         fullWidth={true}
-        label="With normal TextField"
+        label="Transaction Title"
         id="filled-start-adornment"
-        sx={{ mt: "22px" , display:"block" }}
+        sx={{ mt: "22px", display: "block" }}
         slotProps={{
           input: {
             startAdornment: (
@@ -30,23 +35,36 @@ const Create = () => {
       />
 
       <TextField
+        onChange={(eo) => {
+          // @ts-ignore
+          setprice(Number((eo.target.value)));
+        }}
         fullWidth={true}
-        label="With normal TextField"
+        label="Amount"
         id="filled-start-adornment"
-        sx={{ mt: "22px" , display:"block" }}
+        sx={{ mt: "22px", display: "block" }}
         slotProps={{
           input: {
-            startAdornment: (
-              <InputAdornment position="start">$</InputAdornment>
-            ),
+            startAdornment: <InputAdornment position="start">$</InputAdornment>,
           },
         }}
         variant="filled"
       />
 
-      <ColorButton sx={{mt:"22px"}} variant="contained">
-      SUBMIT <KeyboardArrowRightIcon/>
-
+      <ColorButton
+        onClick={() => {
+          fetch("http://localhost:3100/mydata", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({title , price}),
+          });
+        }}
+        sx={{ mt: "22px" }}
+        variant="contained"
+      >
+        SUBMIT <KeyboardArrowRightIcon />
       </ColorButton>
     </Box>
   );
